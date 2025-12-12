11104605c7dd594e08ff841fdf3629aae5451f91
Alexandr Paramonov <paramonovsasha@icloud.com>
2025-12-13 00:50:08 +0300

    project update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -16,6 +17,12 @@ module.exports = {
       },
     }],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
   devServer: {
     static: './dist',
     open: true,
